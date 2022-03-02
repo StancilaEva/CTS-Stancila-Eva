900e2f2bd636214bffbac2b0e6cfd42ada60a46a
Eva Maria <stancilaeva@gmail.com>
2022-03-02 19:52:15 +0200

    Seminar 2

---
+++ seminar2/src/seminar2/AngajatReader.java
@@ -0,0 +1,28 @@
+package seminar2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AngajatReader extends AplicantReader{
+    @Override
+    public List<Aplicant> readAplicanti(String file) throws FileNotFoundException {
+        Scanner input2 = new Scanner(new File(file));
+        input2.useDelimiter(",");
+        List<Aplicant> angajati = new ArrayList<Aplicant>();
+
+        while (input2.hasNext()) {
+            Angajat a = new Angajat();
+            citesteAplicant(input2,a);
+            int salariu = input2.nextInt();
+            String ocupatie = input2.next();
+            a.setSalariu(salariu);
+            a.setOcupatie(ocupatie);
+            angajati.add(a);
+        }
+        input2.close();
+        return angajati;
+    }
+}
---
+++ seminar2/src/seminar2/StudentReader.java
@@ -0,0 +1,28 @@
+package seminar2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StudentReader extends AplicantReader{
+    @Override
+    public List<Aplicant> readAplicanti(String file) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(file));
+        input.useDelimiter(",|\n");
+        List<Aplicant> studenti = new ArrayList<>();
+
+        while (input.hasNext()) {
+            Student student = new Student();
+            citesteAplicant(input,student);
+            int an_studii = input.nextInt();
+            String facultate = (input.next()).toString();
+            student.setAn_studii(an_studii);
+            student.setFacultate(facultate);
+            studenti.add(student);
+        }
+        input.close();
+        return studenti;
+    }
+}
